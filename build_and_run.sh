081f0eb0a2a608d4fe60e3609ee5aefa6d73c6b0
Chris Bradel <ChrisBradel14@gmail.com>
2021-03-23 04:41:34 -0400

    tweak

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,4 @@
 set x
 make -j
-"C:\devkitPro\tools\bin\nxlink.exe" -a 192.168.1.160 -s Brick-Game-9999-in-1.nro
+"C:\devkitPro\tools\bin\nxlink.exe" -a 192.168.1.160 -r 20 -s "Brick-Game-9999-in-1.nro"
+echo "Success"
