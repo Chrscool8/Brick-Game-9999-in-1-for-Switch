5d854f221dee07205e292c103464be4c756abbe9
Chris Bradel <ChrisBradel14@gmail.com>
2021-03-23 08:54:39 -0400

    basic grid

--- source/demo.h
+++ source/demo.h
@@ -7,13 +7,14 @@ extern "C" {
 
 struct DemoData {
     int fontNormal, fontBold, fontIcons, fontEmoji;
-    int images[12];
+    int images[14];
 };
 typedef struct DemoData DemoData;
 
 int loadDemoData(NVGcontext* vg, DemoData* data);
 void freeDemoData(NVGcontext* vg, DemoData* data);
 void renderDemo(NVGcontext* vg, float mx, float my, float width, float height, float t, int blowup, DemoData* data);
+void renderTester(NVGcontext* vg, float mx, float my, float width, float height, float t, int blowup, DemoData* data);
 
 #ifdef __cplusplus
 }
